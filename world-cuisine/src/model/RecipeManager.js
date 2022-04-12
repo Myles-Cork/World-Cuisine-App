d505b05a87e6ce781262bf0090653367650f472a
Myles-Cork <38143953+Myles-Cork@users.noreply.github.com>
2022-04-11 21:58:13 -0600

    Fix recipe document converter

--- world-cuisine/src/model/RecipeManager.js
+++ world-cuisine/src/model/RecipeManager.js
@@ -1,5 +1,5 @@
 import Recipe from "./Recipe";
-import { collection, addDoc, setDoc, getDocs, getDoc, doc, collectionGroup, query, where, withConverter } from "firebase/firestore";
+import { collection, addDoc, setDoc, getDocs, getDoc, doc } from "firebase/firestore";
 import { db } from "../scripts/firebaseUtils";
 import SpoonacularAdapter from "../scripts/SpoonacularAdapter";
 
@@ -14,8 +14,6 @@ const recipeConverter = {
     },
     fromFirestore: (snapshot, options) => {
         const data = snapshot.data(options);
-        console.log("Converting from firestore!");
-        console.log(data);
         return new Recipe(data.id, data.title, data.image);
     }
 };
@@ -79,33 +77,15 @@ class RecipeManager {
     // }
 
     static async queryCuisine(cuisine){
-        // Check if in Firebase
-        //let recipes = [];
-        const recipeSubcollection = collection(db, 'recipes', cuisine, 'recipes').withConverter(recipeConverter);
+        let collectionref = collection(db, 'recipes', cuisine, 'recipes').withConverter(recipeConverter);
 
-        let recipes = getDocs(recipeSubcollection)
-        .then( (querySnapshot) => {
-            recipes = querySnapshot.docs.map(doc => doc.get().data());
-            console.log(recipes);
-        })
-        
+        let recipes;
+        await getDocs(collectionref)
+        .then(async (querySnapshot) => {
+            recipes = querySnapshot.docs.map(doc => doc.data());
+        });
 
-        //const q = query(recipeSubcollection, where("id", "==", 632003));
-        //const recipesFromDBSnapshot = await getDocs(recipeSubcollection);
-        // console.log(recipesFromDBSnapshot);
-        // recipesFromDBSnapshot.forEach(async (doc) => {
-        //     // console.log(doc.id, " => ", doc.data());
-        //     let recipe = (await doc.get()).data();
-        //     console.log(recipe);
-        //     recipes.push(recipe);
-        //     // const recipe = (await )
-        //     // console.log(await (doc.get()).data());
-        //     // recipes.push(doc.withConverter(recipeConverter));
-        //     // console.log(recipes);
-        // }).then({
-        // console.log(recipes);
-        // return recipes;
-        //return this.fillAsyncArray(recipesFromDBSnapshot);
+        return recipes;
     }
 
         // if(recipesFromDB.length > 0){
